94541cab20979c9821e85cc96b5b17b48a30ab84
yezanee <rbfrk02@naver.com>
2025-03-18 17:29:50 +0900

    Fix: 충돌 에러 해결

--- src/main/java/dev/auth/controller/MovieController.java
+++ src/main/java/dev/auth/controller/MovieController.java
@@ -1,21 +1,16 @@
 package dev.auth.controller;
 
-import java.util.List;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import dev.auth.dto.MovieUpdateRequestDTO;
 import dev.auth.dto.MovieResponseDTO;
+import dev.auth.dto.MovieUpdateRequestDTO;
+import dev.auth.model.Movie;
 import dev.auth.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/movies")
--- src/main/java/dev/auth/model/Movie.java
+++ src/main/java/dev/auth/model/Movie.java
@@ -1,25 +1,12 @@
 package dev.auth.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-package dev.auth.model;
-
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
