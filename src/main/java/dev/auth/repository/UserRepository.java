e4821e186c772e969ba9a186fab0bc614df070e0
SYJ_MSI <tom990422@gmail.com>
2025-03-18 17:59:14 +0900

    hotfix

--- src/main/java/dev/auth/repository/UserRepository.java
+++ src/main/java/dev/auth/repository/UserRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.auth.model.User;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findUserByUsername(String username);
     boolean existsByUsername(String username);
 }
